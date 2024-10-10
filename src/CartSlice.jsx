90bf3157f472db1092c3ddb76573fac8a813f2e6
Yulia K <yulia@gmail.com>
2024-10-10 12:29:42 +0300

    iterate plants

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,11 +3,23 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [], 
   },
   reducers: {
     addItem: (state, action) => {
-    
+      if(state.items.filter((item) => item.name === action.payload.name).length === 0) {
+        state.items.push(action.payload);
+      } else {
+        state.items = state.items.map((item) => {
+          if(item.name === action.payload.name) {
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+            };
+          }
+          return item;
+        });
+      }
     },
     removeItem: (state, action) => {
     },
